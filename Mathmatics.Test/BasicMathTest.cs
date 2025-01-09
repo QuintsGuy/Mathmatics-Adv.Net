f3d106a0e15786d02211154c54354cc63740b572
QuintsGuy <knotttristan@gmail.com>
2025-01-09 18:49:47 -0500

    Basic Math Functions and testing

--- Mathmatics.Test/BasicMathTest.cs
+++ Mathmatics.Test/BasicMathTest.cs
@@ -3,8 +3,34 @@ namespace Mathmatics.Test;
 public class BasicMathTest
 {
     [Fact]
-    public void Test1()
+    public void TestAddTwoNumbers()
     {
-        
+        BasicMath basicMath = new BasicMath();
+        int result = basicMath.Add(5, 3);
+        Assert.Equal(8, result);
+    }
+    
+    [Fact]
+    public void TestSubtractTwoNumbers()
+    {
+        BasicMath basicMath = new BasicMath();
+        int result = basicMath.Subtract(5, 3);
+        Assert.Equal(2, result);
+    }
+    
+    [Fact]
+    public void TestMultiplyTwoNumbers()
+    {
+        BasicMath basicMath = new BasicMath();
+        int result = basicMath.Multiply(5, 3);
+        Assert.Equal(15, result);
+    }
+    
+    [Fact]
+    public void TestDivideTwoNumbers()
+    {
+        BasicMath basicMath = new BasicMath();
+        int result = basicMath.Divide(6, 3);
+        Assert.Equal(2, result);
     }
 }
